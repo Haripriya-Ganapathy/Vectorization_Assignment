ed6243c4fda510a34e30ee8d024f0e334d3099cb
G S HARIPRIYA <2020cs0008@svce.ac.in>
2024-01-18 16:19:27 +0530

    Vector 128-bit and 256-bit Addition for INT and FLOAT Data types

--- Addition/add_float.cpp
+++ Addition/add_float.cpp
@@ -1,40 +1,65 @@
 # include <iostream>
 # include <chrono>
+# include <iomanip>
 # include <immintrin.h>
 
 void Scalar_Addition (float arr1[] , float arr2[] , float result1[] , int size ) {
 
-   int i;
-   for( i = 0; i < size ; i++ ){
+   for( int i = 0; i < size ; i++ ){
        // Add 2 arrays element wise
        result1 [i] = arr1 [i] + arr2 [i] ;
    }
 
 }
 
-void Vector_Addition (float arr1[] , float arr2[] , float result2[] , int size ) {
+void Vector_Addition_128 (float arr1[] , float arr2[] , float result2[] , int size ) {
 
-    int i;
-    for ( i = 0; i < size; i += 4) {
+    // Process 4 elements at a time using AVX2 intrinsics
+    for ( int i = 0; i < size - 3 ; i += 4) {
         // Load 128-bit vectors from arr1 and arr2
-        __m128 vector1 = _mm_loadu_ps(&arr1[i]);
-        __m128 vector2 = _mm_loadu_ps(&arr2[i]);
+        __m128 vector1 = _mm_loadu_ps(arr1 + i);
+        __m128 vector2 = _mm_loadu_ps(arr2 + i);
 
         // Perform 128-bit float addition
         __m128 sum = _mm_add_ps(vector1, vector2);
 
         // Store the result back to memory
-        _mm_storeu_ps(&result2[i], sum);
-    
+        _mm_storeu_ps((result2 + i), sum);
+    }
+
+    // Handle the remaining elements using scalar operations
+    for (int i = size - (size % 4); i < size; ++i) {
+        result2[i] = arr1[i] + arr2[i];
     }
 
 }
 
-void Compare_Results(float result1[] , float result2[] , int size){
+void Vector_Addition_256 (float arr1[] , float arr2[] , float result3[] , int size ) {
 
-    int i;
-    for( i = 0; i < size ; i++){
-        if(result1[i] != result2[i]){
+    // Process 8 elements at a time using AVX2 intrinsics
+    for ( int i = 0; i < size - 7; i += 8) {
+        // Load 256-bit vectors from arr1 and arr2
+        __m256 vector1 = _mm256_loadu_ps(arr1 + i);
+        __m256 vector2 = _mm256_loadu_ps(arr2 + i);
+
+        // Perform 256-bit float addition
+        __m256 sum =  _mm256_add_ps (vector1, vector2);
+
+        // Store the result back to memory
+        _mm256_storeu_ps(result3 + i , sum);
+    }
+
+    // Handle the remaining elements using scalar operations
+    for (int i = size - (size % 8); i < size; ++i) {
+        result3[i] = arr1[i] + arr2[i];
+    }
+
+}
+
+void Compare_Results(float result1[] , float result2[] , float result3[] , int size){
+
+    for( int i = 0; i < size ; i++){
+        if(result1[i] != result2[i] && (result1[i] != result3[i])){
             std::cout<<"Both scalar and vector float addition results are not equal!\n";
             return;
         }
@@ -45,36 +70,53 @@ void Compare_Results(float result1[] , float result2[] , int size){
 
 int main(){
 
-    int size = 4, i;
-    float arr1[] = {1.21,2.41,3.89,4.76} , arr2[] = {5.33,6.78,7.12,8.23} , result1[size] , result2[size];
+    int size = 17;
+    float arr1[] = {1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9,9.0,10.1,11.2,12.3,13.4,14.5,15.6,16.7,17.8} , arr2[] = {18.9,19.0,20.1,21.2,22.3,23.4,24.5,25.6,26.7,27.8,28.9,29.0,30.1,31.2,32.3,33.4,34.5} , result1[size] , result2[size] , result3[size];
+    
     std::chrono::time_point<std::chrono::high_resolution_clock > start, end;
+    std::chrono::duration<double> scalar_time(0), vector_time(0) , avg_scalar_time , avg_vector_time;
  
     // Scalar Float Addition
-    start = std::chrono::high_resolution_clock ::now();
     for (int i = 0; i < 1000000; ++i) {
         // This loop is just to calculate the small timing durations 
+        start = std::chrono::high_resolution_clock ::now();
         Scalar_Addition(arr1 , arr2 , result1 , size);
+        end = std::chrono::high_resolution_clock ::now();
+        scalar_time += (end - start);
     }
-    end = std::chrono::high_resolution_clock ::now();
-
+    
     // Execution Time for Scalar Float Addition
-    std::chrono::duration<double> scalar_time = end - start;
-    std::cout<<"\nExecution Time for Scalar float Addition : "<<scalar_time.count()<<" microseconds\n" <<std::endl;
+    avg_scalar_time = scalar_time / 1000000;
+    std::cout<<"\nExecution Time for Scalar float Addition\t : "<< std::setprecision(4) << avg_scalar_time.count()<<" microseconds\n" <<std::endl;
 
-    // Vector Float Addition
-    start = std::chrono::high_resolution_clock ::now();
+    // Vector 128-bit Float Addition
     for (int i = 0; i < 1000000; ++i) {
         // This loop is just to calculate the small timing durations 
-        Vector_Addition(arr1 , arr2 , result2 , size);
+        start = std::chrono::high_resolution_clock ::now();
+        Vector_Addition_128(arr1 , arr2 , result2 , size);
+        end = std::chrono::high_resolution_clock ::now();
+        vector_time += (end - start);
     }
-    end = std::chrono::high_resolution_clock ::now();
 
-    // Execution Time for Vector Addition
-    std::chrono::duration<double> vector_time = end - start;
-    std::cout<<"Execution Time for Vector float Addition : "<<vector_time.count()<<" microseconds\n" <<std::endl;
+    // Execution Time for Vector 128-bit Float Addition
+    avg_vector_time = vector_time / 1000000;
+    std::cout<<"Execution Time for Vector 128-bit float Addition : "<< std::setprecision(4) << avg_vector_time.count()<<" microseconds\n" <<std::endl;
+
+    // Vector 256-bit Float Addition
+    for (int i = 0; i < 1000000; ++i) {
+        // This loop is just to calculate the small timing durations 
+        start = std::chrono::high_resolution_clock ::now();
+        Vector_Addition_256(arr1 , arr2 , result3 , size);
+        end = std::chrono::high_resolution_clock ::now();
+        vector_time += (end - start);
+    }
+
+    // Execution Time for Vector 256-bit Float Addition
+    avg_vector_time = vector_time / 1000000;
+    std::cout<<"Execution Time for Vector 256-bit float Addition : "<< std::setprecision(4) << avg_vector_time.count()<<" microseconds\n" <<std::endl;
 
     // Validating the accuracy of Scalar and Vector Float Addition
-    Compare_Results(result1 , result2 , size);
+    Compare_Results(result1 , result2 , result3 , size);
     
     return 0;
 }
--- Addition/add_int.cpp
+++ Addition/add_int.cpp
@@ -1,78 +1,120 @@
 # include <iostream>
 # include <chrono>
+# include <iomanip>
 # include <immintrin.h>
 
 void Scalar_Addition (int arr1[] , int arr2[] , int result1[] , int size ) {
 
-   int i;
-   for( i = 0; i < size ; i++ ){
+   for( int i = 0; i < size ; i++ ){
        // Add 2 arrays element wise
        result1 [i] = arr1 [i] + arr2 [i] ;
    }
 
 }
 
-void Vector_Addition (int arr1[] , int arr2[] , int result2[] , int size ) {
+void Vector_Addition_128 (int arr1[] , int arr2[] , int result2[] , int size ) {
 
-    int i;
-    for ( i = 0; i < size; i += 4) {
-        __m128i vector1 = _mm_loadu_si128((__m128i*)&arr1[i]);
-        __m128i vector2 = _mm_loadu_si128((__m128i*)&arr2[i]);
+    // Process 4 elements at a time using AVX2 intrinsics
+    for ( int i = 0; i < size - 3; i += 4) {
+        __m128i vector1 = _mm_loadu_si128((__m128i*)(arr1 + i));
+        __m128i vector2 = _mm_loadu_si128((__m128i*)(arr2 + i));
 
         // Perform 128-bit integer addition
         __m128i sum = _mm_add_epi32(vector1, vector2);
 
         // Store the result back to memory
-        _mm_storeu_si128((__m128i*)&result2[i], sum);
+        _mm_storeu_si128((__m128i*)(result2 + i), sum);
+    }
+
+    // Handle the remaining elements using scalar operations
+    for (int i = size - (size % 4); i < size; ++i) {
+        result2[i] = arr1[i] + arr2[i];
     }
 
 }
 
-void Compare_Results(int result1[] , int result2[] , int size){
+void Vector_Addition_256 (int arr1[] , int arr2[] , int result3[] , int size ) {
 
-    int i;
-    for( i = 0; i < size ; i++){
-        if(result1[i] != result2[i]){
+    // Process 8 elements at a time using AVX2 intrinsics
+    for ( int i = 0; i < size - 7; i += 8) {
+        // Load 256-bit vectors from arr1 and arr2
+        __m256i vector1 = _mm256_loadu_si256(( __m256i*)(arr1 + i));
+        __m256i vector2 = _mm256_loadu_si256(( __m256i*)(arr2 + i));
+
+        // Perform 256-bit integer addition
+        __m256i sum = _mm256_add_epi32(vector1, vector2);
+
+        // Store the result back to memory
+        _mm256_storeu_si256(( __m256i*)(result3 + i), sum);
+    }
+    
+    // Handle the remaining elements using scalar operations
+    for ( int i = size - (size % 8); i < size; ++i) {
+        result3[i] = arr1[i] + arr2[i];
+    }
+    
+}
+
+void Compare_Results(int result1[] , int result2[] , int result3[] , int size){
+
+    for( int i = 0; i < size ; i++){
+        if((result1[i] != result2[i]) && (result1[i] != result3[i])){
             std::cout<<"Both scalar and vector addition results are not equal!\n";
             return;
         }
     }
     std::cout<<"Both scalar and vector addition results are equal!\n";
-
 }
 
 int main(){
 
-    int size = 4, i;
-    int arr1[] = {1,2,3,4} , arr2[] = {5,6,7,8} , result1[size] , result2[size];
+    int size = 17;
+    int arr1[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17} , arr2[] = {18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34} , result1[size] , result2[size] , result3[size];
+    
     std::chrono::time_point<std::chrono::high_resolution_clock > start, end;
-
+    std::chrono::duration<double> scalar_time(0), vector_time(0) , avg_scalar_time , avg_vector_time;
+    
     // Scalar Addition
-    start = std::chrono::high_resolution_clock ::now();
     for (int i = 0; i < 1000000; ++i) {
-        // This loop is just to calculate the small timing durations 
+        // This loop is just to calculate the small timing durations
+        start = std::chrono::high_resolution_clock ::now(); 
         Scalar_Addition(arr1 , arr2 , result1 , size);
+        end = std::chrono::high_resolution_clock ::now();
+        scalar_time += (end - start);
     }
-    end = std::chrono::high_resolution_clock ::now();
     
     // Execution Time for Scalar Addition
-    std::chrono::duration<double> scalar_time = end - start;
-    std::cout << "Execution Time for Scalar Addition : " << scalar_time.count() << " microseconds\n" << std::endl;
+    avg_scalar_time = scalar_time / 1000000;
+    std::cout << "\nExecution Time for Scalar Integer Addition\t   : " <<  std::setprecision(4) << avg_scalar_time.count() << " microseconds\n" << std::endl;
 
-    // Vector Addition
-    start = std::chrono::high_resolution_clock ::now();
+    // Vector 128-bit Integer Addition
     for (int i = 0; i < 1000000; ++i) {
         // This loop is just to calculate the small timing durations 
-        Vector_Addition(arr1 , arr2 , result2 , size);
+        start = std::chrono::high_resolution_clock ::now();
+        Vector_Addition_128(arr1 , arr2 , result2 , size);
+        end = std::chrono::high_resolution_clock ::now();
+        vector_time += end - start;
     }
-    end = std::chrono::high_resolution_clock ::now();
     
-    // Execution Time for Vector Addition
-    std::chrono::duration<double> vector_time = end - start;
-    std::cout << "Execution Time for Vector Addition : " << vector_time.count() << " microseconds\n" << std::endl;
+    // Execution Time for Vector 128-bit Integer Addition
+    avg_vector_time = vector_time / 100000;
+    std::cout << "Execution Time for Vector 128-bit Integer Addition : " << std::setprecision(4) << avg_vector_time.count() << " microseconds\n" << std::endl;
+    
+    // Vector 256-bit Integer Addition
+    for (int i = 0; i < 1000000; ++i) {
+        // This loop is just to calculate the small timing durations 
+        start = std::chrono::high_resolution_clock ::now();
+        Vector_Addition_256(arr1 , arr2 , result3 , size);
+        end = std::chrono::high_resolution_clock ::now();
+        vector_time += end - start;
+    }
+
+    // Execution Time for Vector 256-bit Addition
+    avg_vector_time = vector_time / 100000;
+    std::cout << "Execution Time for Vector 256-bit Integer Addition : " << std::setprecision(4) << avg_vector_time.count() << " microseconds\n" << std::endl;
 
     // Validating the accuracy of Scalar and Vector Addition
-    Compare_Results(result1 , result2 , size);
+    Compare_Results(result1 , result2 , result3 , size);
     
     return 0;
 }
