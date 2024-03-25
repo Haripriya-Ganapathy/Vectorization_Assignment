19f11aebff0121f05551170b9d791a7f3efcac04
G S HARIPRIYA <2020cs0008@svce.ac.in>
2024-03-25 14:31:12 +0530

    Modified matrix multiplication (generic code)

--- ARM_Neon_Intrinsics/matrix.cpp
+++ ARM_Neon_Intrinsics/matrix.cpp
@@ -1,122 +1,134 @@
-# include <iostream>
-# include <arm_neon.h>
-# include <chrono>
+#include<iostream>
+#include<arm_neon.h>
+#include <chrono>
+#define ROW1 5
+#define COL1 8 //ROW2=COL1
+#define COL2 3
 
-void print_results(float res[8][8]){
+// void check(float32x4_t out){
 
-    int i, j;
-    for(i = 0; i < 8; i++){
-       for(j = 0; j < 8; j++){
-            std::cout<<res[i][j]<<" ";
-       }
-       std::cout<<"\n";
+//   for(int i=0;i<4;i++){
+//     std::cout<<out[i]<<" ";
+//   }
+//   std::cout<<std::endl;
+// }
+
+void print(float *C,int m, int n){
+    for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        std::cout<<"\t"<<C[i * n + j];
     }
-
+    std::cout<<std::endl;
+  }
 }
 
-void Scalar_matrix_multiply(float mat1[8][8] , float mat2[8][8] , float result1[8][8]){
 
-    int i , j , k;
+void initialize(float *arr, int m, int n) {
+  int i, j;
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < n; j++) {
+      *((arr + i * n) + j) = i * 1.0f;
+    }
+  }
+}
 
-    // Initializing elements of matrix result1 to 0.
-    for(i = 0; i < 8; ++i){
-        for(j = 0; j < 8; ++j){
-            result1[i][j]=0;
+void initialize_Zero(float *arr, int m, int n) {
+  int i, j;
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < n; j++) {
+      *((arr + i * n) + j) = 0.0f;
+    }
+  }
+}
+
+void matmul_scalar(float *A, float *B, float *result) {
+  for (int i = 0; i < ROW1; i++) {
+    for (int j = 0; j < COL2; j++) {
+      *((result + i * COL2) + j) = 0.0f;
+      for (int k = 0; k < COL1; k++) {
+        *((result + i * COL2) + j) +=
+            (*((A + i * COL1) + k) * *((B + k * COL2) + j));
+      }
+    }
+  }
+}
+
+void matmul_intrinsic(float *A, float *B, float *C) {
+
+    float32x4_t a, b,c0;
+    int i,j,k;
+    for ( i = 0; i < ROW1; i ++) {
+            
+        for (j = 0; j <= COL2 - 3; j +=4) {
+                c0 = vdupq_n_f32(0.0);
+
+                for(k = 0; k < COL1; k++){
+                    // Load the values from the matrices into SSE registers
+                    a = vdupq_n_f32(*(A + i*COL1 + k));
+                    b = vld1q_f32(&(*(B + k*COL2 + j)));
+                
+                    // check(a);
+                    // check(b);
+
+                    // Perform SIMD multiplication
+                    c0 = vaddq_f32(c0, vmulq_f32(a,b));
+                    // check(c0);
+                }
+                // Store the results back into memory
+                vst1q_f32(&(*(C + i*COL2 + j)), c0);
         }
-    }
 
-    for(i = 0; i < 8; ++i)
-        for(j = 0; j < 8; ++j)   
-            for(k = 0; k < 8; ++k)
-            {
-                // Multiplying  row from mat1 and first colum from mat2 and summing up 
-                result1[i][j] += mat1[i][k] * mat2[k][j];
-            }
-
-}
-
-
-
-void Vector_matrix_multiply_32x4(float mat1[8][8] , float mat2[8][8] , float result1[8][8] ){
-
-    int i , j , k;  
-    float32x4_t a , b, sum;
-
-    for(i = 0; i < 8; i++){
-        for(j = 0; j < 8; j++){
-            sum = vdupq_n_f32(0.0);
-            for(k = 0; k < 8; k++){
-                a = vdupq_n_f32(mat1[i][k]);
-                b = vld1q_f32(&mat2[k][j]);
-                sum =  vaddq_f32(sum, vmulq_f32(a, b));
-            }
-            vst1q_f32(&result1[i][j], sum);
+        // To handle remaining elements where matrix size is not a multiple of 4
+        while(j < COL2){
+           for(k = 0 ;k < COL1; k++){
+              *(C + i*COL2 + j) += (*(A + i*COL1 + k) * (*(B + k*COL2 + j)));
+           }
+          j++;
         }
+        
     }
 
 }
-
 int main(){
-   
-    int size = 8;
-    // Input arrays
-    float mat1[8][8] = {
-        {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0},
-        {2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0},
-        {3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0},
-        {4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0},
-        {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0},
-        {6.0, 6.0, 6.0, 6.0, 6.0, 6.0, 6.0, 6.0},
-        {7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0},
-        {8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0}
-    };
-    float mat2[8][8] = {
-        {8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0, 8.0},
-        {7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0},
-        {6.0, 6.0, 6.0, 6.0, 6.0, 6.0, 6.0, 6.0},
-        {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0},
-        {4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0},
-        {3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0},
-        {2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0},
-        {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}
-    };
 
-    float result1[8][8] , result2[8][8]  ;
+  float *A = (float *)malloc(sizeof(float) * ROW1 * COL1);
+  float *B = (float *)malloc(sizeof(float) * COL1 * COL2);
+  float *C = (float *)malloc(sizeof(float) * ROW1 * COL2);
+  float *D = (float *)malloc(sizeof(float) * ROW1 * COL2);
 
-/* Vectorize custom_abs function */
-    std::cout<<"\nCalling custom_abs function";
-    auto start = std::chrono::high_resolution_clock::now();
-    Scalar_matrix_multiply(mat1 , mat2 , result1);
-    print_results(result1);
-    auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> scalar_time, vector_time;
 
+  initialize(A, ROW1, COL1);
+  initialize(B, COL1, COL2);
+  initialize_Zero(C, ROW1, COL2);
+  initialize_Zero(D, ROW1, COL2);
 
-    // Execution Time for Scalar custom_abs Funtion
-    std::chrono::duration<double> scalar_time = end - start;
-    std::cout<<"\nExecution Time for Scalar custom_abs Funtion\t\t : "<<  scalar_time.count()<<" microseconds\n" <<std::endl;
+  print(A,ROW1,COL1);
+  std::cout<<std::endl;
 
-    start = std::chrono::high_resolution_clock::now();
-    Vector_matrix_multiply_32x4(mat1, mat2, result2);
-    end = std::chrono::high_resolution_clock::now();
+  print(B,COL1,COL2);
+  std::cout<<std::endl;
 
-    std::chrono::duration<double> vector_time = end - start;
-    print_results(result2);
-    // double v_time = std::chrono::duration_cast<std::chrono::microseconds>(vector_time).count();
-    std::cout<<"\nExecution Time for Scalar custom_abs Funtion\t\t : "<<  vector_time.count()<<" microseconds\n" <<std::endl;
+  auto start = std::chrono::high_resolution_clock::now();
+  matmul_scalar(A,B,C);
+  auto end = std::chrono::high_resolution_clock::now();
+  scalar_time = end - start;
 
-    
-    double performance1 = ((scalar_time.count() - vector_time.count()) / scalar_time.count()) * 100 ;
-    std::cout <<"\nVector 256-bit Integer Addition is " << performance1 <<" \% faster than scalar\n";
+  print(C,ROW1,COL2);
+  std::cout<<std::endl;
 
-    return 0;
+  std::cout<<scalar_time.count()<<std::endl;
+
+  start = std::chrono::high_resolution_clock::now();
+  matmul_intrinsic(A, B, D);
+  end = std::chrono::high_resolution_clock::now();
+  vector_time = end - start;
+
+  print(D,ROW1,COL2);
+  std::cout<<std::endl;
+
+  std::cout<<vector_time.count()<<std::endl;
+  
+  return 0;
+
 }
-
-
-/*void print_output(float out[], int size){
-
-    int i;
-    for(i = 0; i < size; i++){
-        std::cout<<out[i]<<" ";
-    }
-    std::cout<<"\n";
-}*/
